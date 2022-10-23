784e9c2e278303a4fa218ba5bdedf287b6ec86ae
Nataly Evpak <natalyevpak@gmail.com>
2022-10-23 13:12:00 +0300

    terraform files

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+/*
+
+output "access_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  sensitive = true
+}
+
+output "secret_key" {
+  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  sensitive = true
+}
+
+*/
+
+#terraform output -raw secret_key
+#terraforn output -raw access_key
+
