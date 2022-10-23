aea63d43b93a5563f93a3d62f6904ef9495e76fa
Nataly Evpak <natalyevpak@gmail.com>
2022-10-23 10:34:22 +0300

    added main terrafor file

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.81.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token                    = "auth_token_here"
+  service_account_key_file = "path_to_service_account_key_file"
+  cloud_id                 = "cloud_id_here"
+  folder_id                = "folder_id_here"
+  zone                     = "ru-central1-a"
+}
